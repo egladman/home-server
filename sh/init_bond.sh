65576197a445b28be59477c5d02987c2f03986c4
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-11-01 22:32:01 -0400

    Create init_bond.sh

---
+++ sh/init_bond.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Usage: init_bond.sh <device1> <device2>
+#        init_bond.sh enp6s0 enp7s0
+
+# Bond <n> network interfaces as one logical interface
+
+BOND_DEVICE_MASTER=bond0
+BOND_DEVICE_SLAVES=("$@")
+BOND_DEVICE_SLAVE_PREFIX="${BOND_DEVICE_MASTER}-slave"
+BOND_MODE="802.3ad"
+# balance-rr    (0)
+# active-backup (1)
+# balance-xor   (2)
+# broadcast     (3)
+# 802.3ad       (4)
+# balance-tlb   (5)
+# balance-alb   (6)
+
+_log() {
+    printf -v timestamp '%(%Y%m%d%H%M%S)T' -1
+    printf '%b\n' "INFO ${timestamp} ${0##*/} $*"
+}
+
+bond_list() {
+    modprobe bonding
+    if [[ $? -ne 0 ]]; then
+	_log "Command 'modprobe' returned non-zero code. Kernel module 'bonding' not loaded."
+	exit 1
+    fi
+
+    _log "Listing network interfaces..."
+    lshw -class network -short
+    if [[ $? -ne 0 ]]; then
+	_log "Command 'lshw' returned non-zero code. Failed to list all network interfaces."
+	exit 1
+    fi
+}
+
+bond_delete() {
+    _log "Deleting network interfaces: ${BOND_DEVICE_SLAVES[@]}"
+    nmcli connection del "${BOND_DEVICE_SLAVES[@]}"
+    if [[ $? -ne 0 ]]; then
+	_log "Command 'nmcli' returned non-zero code. Failed to delete devices: ${BOND_DEVICE_SLAVES[@]}"
+    fi
+}
+
+bond_create_and_start() {
+    _log "Creating interface ${BOND_DEVICE_MASTER}"
+    nmcli connection add type bond ifname ${BOND_DEVICE_MASTER} con-name ${BOND_DEVICE_MASTER} mode ${BOND_MODE}
+    if [[ $? -ne 0 ]]; then
+	_log "Command 'nmcli' returned non-zero code. Failed to create ${BOND_DEVICE_MASTER}"
+	exit 1
+    fi
+    
+    for i in "${!BOND_DEVICE_SLAVES[@]}"; do
+	slave_name=${BOND_DEVICE_SLAVE_PREFIX}${i}
+
+	_log "Creating interface ${slave_name}"
+	nmcli connection add type bond-slave ifname ${BOND_DEVICE_SLAVES[$i]} con-name ${slave_name} master ${BOND_DEVICE_MASTER}
+	if [[ $? -ne 0 ]]; then
+	   _log "Command 'nmcli' returned non-zero code. Failed to add slave '${BOND_DEVICE_SLAVES[$i]}' to '${BOND_DEVICE_MASTER}'"
+	   exit 1
+	fi
+
+	_log "Bringing up interface ${slave_name}"
+	nmcli connection up ${slave_name}
+	if [[ $? -ne 0 ]]; then
+	   _log "Command 'nmcli' returned non-zero code. Failed to bring up interface 'slave${i}'"
+	   exit 1
+	fi
+    done
+
+    _log "Bringing up interface ${BOND_DEVICE_MASTER}"
+    nmcli connection up ${BOND_DEVICE_MASTER}
+    if [[ $? -ne 0 ]]; then
+	_log "Command 'nmcli' returned non-zero code. Failed to bring up interface '${BOND_DEVICE_MASTER}'"
+	exit 1
+    fi
+}
+
+main() {
+    bond_list
+    bond_delete
+    bond_create_and_start
+    _log "Completed"
+}
+main
